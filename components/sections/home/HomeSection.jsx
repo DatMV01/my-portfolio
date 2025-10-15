40c912fb4027ecaf4877aa2091805a7e371c815a
DatMV <datmv01@gmail.com>
2025-10-15 11:01:46 +0700

    Enhance HomeSection with new layout and content; add profile image, introduction, and call-to-action buttons; include CV and hand icon assets

--- components/sections/home/HomeSection.jsx
+++ components/sections/home/HomeSection.jsx
@@ -1,17 +1,63 @@
-import Button from "@/components/ui/Button";
+import { ArrowRight, Download } from "lucide-react";
+import clsx from "clsx";
 import React from "react";
-   
+
 const HomeSection = () => {
   return (
-    <section id="home" className="min-h-screen">
-      <div>HomeSection</div>
-      <div className="flex gap-4 flex-wrap">
-        <Button variant="primary">Click Me</Button>
-        <Button variant="secondary">Click Me</Button>
-        <Button variant="outline">Click Me</Button>
-        <Button variant="destructive">Click Me</Button>
-        <Button variant="ghost">Click Me</Button>
-        <Button variant="link">Click Me</Button>
+    <section
+      id="home"
+      className="-mt-16 flex min-h-screen items-center justify-center text-center"
+    >
+      <div className="flex flex-col items-center justify-center gap-2">
+        <img
+          src="/images/profile-img.png"
+          alt="Profile"
+          className="w-32 rounded-full"
+        />
+
+        <h3 className="mb-3 flex items-end gap-2 text-xl md:text-2xl">
+          <span>Hi! I'm Dat Mai</span>
+          <img src="/icons/hand-icon.png" alt="Hand icon" className="w-6" />
+        </h3>
+
+        <h1 className="text-3xl sm:text-6xl lg:text-[66px]">
+          web developer <br /> based in VietNam.
+        </h1>
+
+        <p className="max-w-2xl px-2 text-center text-xl leading-loose">
+          I am a web developer from Ho Chi Minh, VietNam with 1 year of
+          experience in multiple companies like Microsoft, Tesla, and Apple.
+        </p>
+
+        <div className="mt-4 flex flex-col gap-4 md:flex-row">
+          <a
+            href="#contact"
+            className={clsx(
+              "block h-12 w-38",
+              "flex items-center justify-center gap-x-2 rounded-full text-white",
+              "bg-gradient-to-r from-purple-600 to-orange-500",
+              "hover:scale-105 hover:shadow-md",
+              "cursor-pointer transition-transform duration-300",
+            )}
+          >
+            <span>Connect Me</span>
+            <ArrowRight />
+          </a>
+          <a
+            download
+            href="/documents/cv.pdf"
+            className={clsx(
+              "block h-12 w-38",
+              "flex items-center justify-center gap-x-2 rounded-full text-white",
+              "bg-gradient-to-r from-orange-600 to-purple-500",
+              "hover:scale-105 hover:shadow-md",
+              "cursor-pointer transition-transform duration-300",
+            )}
+          >
+            <span>My Resume</span>
+            <Download />
+          </a>
+        </div>
       </div>
     </section>
   );
